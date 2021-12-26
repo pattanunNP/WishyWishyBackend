17525bde663f267355f51aff871eac7c9d9a474a
PattanunNP <32350702+pattanunNP@users.noreply.github.com>
2021-12-27 02:01:13 +0700

    Port Config

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/pattanunNP/wishbackend/config"
 	"github.com/pattanunNP/wishbackend/database"
 	"github.com/pattanunNP/wishbackend/router"
 )
@@ -20,12 +19,10 @@ func main() {
 
 	router.SetupRoutes(app)
 
-	port := config.Config("APP_PORT")
-
 	// Verify if heroku provided the port or not
-	if os.Getenv("PORT") == "" {
-		port = "4000"
-	}
+	port := os.Getenv("PORT")
+	// If port is not provided, use default port
+
 	fmt.Printf("Server started on port 🚀 %s\n", port)
 
 	log.Fatal(app.Listen(":" + port))
