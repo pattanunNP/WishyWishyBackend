4c493a3d5e612511df1000ea569a905f76394b82
pattananNP <phattanun19@hotmail.com>
2022-12-24 22:36:50 +0700

    Fix Mongo

--- database/mongodb.go
+++ database/mongodb.go
@@ -21,7 +21,7 @@ func MongoDB() {
 	// Add your init code here
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(config.Config("MONGO_URL")))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(config.Config("MONGODB_URI")))
 
 	if err != nil {
 		log.Fatal(err)
